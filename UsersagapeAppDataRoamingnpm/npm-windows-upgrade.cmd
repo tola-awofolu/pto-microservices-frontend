d598384c46581b226188a0abd449e6f6a6f2f883
Tola A <serverity.io@gmail.com>
2018-10-03 16:46:47 -0400

    Removed irrelevant folder(s)

--- UsersagapeAppDataRoamingnpm/npm-windows-upgrade.cmd
+++
@@ -1,7 +0,0 @@
-@IF EXIST "%~dp0\node.exe" (
-  "%~dp0\node.exe"  "%~dp0\node_modules\npm-windows-upgrade\bin\npm-windows-upgrade.js" %*
-) ELSE (
-  @SETLOCAL
-  @SET PATHEXT=%PATHEXT:;.JS;=;%
-  node  "%~dp0\node_modules\npm-windows-upgrade\bin\npm-windows-upgrade.js" %*
-)
